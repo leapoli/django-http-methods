1a35ea84c17d7465665abe8d7352a7d4d3c3407e
leapoli <19159524+leapoli@users.noreply.github.com>
2021-10-08 19:46:20 -0300

    Update middleware.py
    Support for PATCH verb

--- middleware.py
+++ middleware.py
@@ -13,6 +13,10 @@ class HttpXMethodOverride:
                 request.method = 'PUT'
                 request.META['REQUEST_METHOD'] = 'PUT'
                 request.PUT = QueryDict(request.body)
+            if http_method.lower() == 'patch':
+                request.method = 'PATCH'
+                request.META['REQUEST_METHOD'] = 'PATCH'
+                request.PATCH = QueryDict(request.body)
             if http_method.lower() == 'delete':
                 request.method = 'DELETE'
                 request.META['REQUEST_METHOD'] = 'DELETE'
